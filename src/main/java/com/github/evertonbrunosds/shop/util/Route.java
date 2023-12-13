8f8aee3ae75ecd2b4d7ecdc00286a1d99b7b1fa0
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-13 11:33:51 -0300

    v0.9 - modelagem de rotas de API e página

---
+++ src/main/java/com/github/evertonbrunosds/shop/util/Route.java
@@ -0,0 +1,11 @@
+package com.github.evertonbrunosds.shop.util;
+
+public final class Route {
+
+    public static final class API {
+        public static final String PRODUCT = "/api/product";
+    }
+
+    public static final class PAGE {}
+
+}
