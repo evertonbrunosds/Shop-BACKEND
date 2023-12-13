719a926f48da8cded3eb3a8179bf3bceea62a9d2
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-13 11:54:07 -0300

    v1.1 - modelagem de visualização 'product'

---
+++ src/main/java/com/github/evertonbrunosds/shop/view/ProductView.java
@@ -0,0 +1,33 @@
+package com.github.evertonbrunosds.shop.view;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.evertonbrunosds.shop.controller.ProductController;
+import com.github.evertonbrunosds.shop.model.entity.ProductEntity;
+import com.github.evertonbrunosds.shop.model.request.ProductRequest;
+import com.github.evertonbrunosds.shop.model.response.ProductResponse;
+import com.github.evertonbrunosds.shop.util.Route;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = Route.API.PRODUCT)
+public class ProductView {
+
+    private final ProductController controller;
+
+    private final ModelMapper mapper;
+
+    @PostMapping
+    public ProductResponse post(final ProductRequest request) {
+        final var entityBeforeSave = mapper.map(request, ProductEntity.class);
+        final var entityAfterSave = controller.post(entityBeforeSave);
+        final var response = mapper.map(entityAfterSave, ProductResponse.class);
+        return response;
+    }
+
+}
